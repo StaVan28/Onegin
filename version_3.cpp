ae4c032c84bbc8d46f9bc653e9df07002318b8c0
StaVan28 <70322912+StaVan28@users.noreply.github.com>
2020-11-13 21:39:29 +0300

    Update version_3.cpp

--- version_3.cpp
+++ version_3.cpp
@@ -6,8 +6,6 @@
 #include <assert.h>
 #include <locale.h>
 #include <ctype.h>
-//errno - gloval var
-//perror
 
 //-----------------------------------------------------------------------------
 
@@ -21,7 +19,7 @@ enum Errors
      FILLING_STRUCTURES_ERROR ,
      COMPILER_INPUT_ERROR     ,
      CHARACTER_COUNT_ERROR    ,
-     STRINGS_COUNT_ERROR      
+     STRINGS_COUNT_ERROR
 };
 
 //-----------------------------------------------------------------------------
@@ -35,9 +33,9 @@ struct line_t
 //-----------------------------------------------------------------------------
 
 size_t NumberOfSimbols(FILE* fp);
-size_t NumberOfStrings(size_t simbols, char* buffer);
-//названия переменных
-char* MakeBuffer(FILE* in, size_t simbols);
+size_t NumberOfStrings(size_t n_symbols, char* buffer);
+
+char* MakeBuffer(FILE* in, size_t n_symbols);
 
 size_t SortFile(FILE* in, FILE* out, const char* mode);
 size_t FillingStructs(char* buffer, line_t* arrstr);
@@ -58,7 +56,7 @@ int main()
 
     FILE* in  = fopen("onegin.txt", "rb");
 	if(!in)  return FILE_OPENING_ERROR;
-	
+
 	//!TODO if (errno != 0) perror (errno, "Error on line %d", __LINE__);
 
 	FILE* out = fopen("result.txt", "wb");
@@ -66,7 +64,7 @@ int main()
 
 //You can choose three types of sorting: "alphabet++" (sort ascending),
 //"alphabet--" (descending sort), "rhyme" (sorting by rhyme)
-	SortFile(in, out, "rhyme");
+	SortFile(in, out, "alphabet++");
 
 	fclose(in);
 	if(!in)  return FILE_CLOSING_ERROR;
@@ -79,19 +77,9 @@ int main()
 
 //*****************************************************************************
 
-inline void print_hello(void)
-{
-    printf("    Hello!\n You can choose three types of sorting:\n");
-    printf(" alphabet++ (sort ascending),\n");
-    printf(" alphabet-- (descending sort),\n rhyme      (sorting by rhyme).\n\n");
-    printf("    Please choose one:\n");
-}
-
-//-----------------------------------------------------------------------------
-
 size_t NumberOfSimbols(FILE* fp)
 {
-    assert(fp);
+    if(!fp) return FILE_OPENING_ERROR;
 
     size_t start_value = ftell(fp);
 
@@ -104,32 +92,32 @@ size_t NumberOfSimbols(FILE* fp)
 
 //-----------------------------------------------------------------------------
 
-size_t NumberOfStrings(size_t simbols, char* buffer)
-{//!TODO FIX VARIABLE'S NAME
-    assert(simbols);
-    assert(buffer);
+size_t NumberOfStrings(size_t n_symbols, char* buffer)
+{
+	if(!n_symbols)        return CHARACTER_COUNT_ERROR;
+	if(buffer == nullptr) return MEMORY_ALLOCATION_ERROR;
 
-	size_t strings = 0;
+	size_t n_strings = 0;
 
-	for (size_t i = 0; i < simbols; i++)
-        if (buffer[i] == '\n')
-            strings++;
+	for (size_t index = 0; index < n_symbols; index++)
+        if (buffer[index] == '\n')
+            n_strings++;
 
-	return strings;
+	return n_strings;
 }
 
 //-----------------------------------------------------------------------------
 
-char* MakeBuffer(FILE* in, size_t simbols)
+char* MakeBuffer(FILE* in, size_t n_symbols)
 {
     assert(in);
-    assert(simbols);
+    assert(n_symbols);
 
-	char*  buffer = (char*) calloc(simbols + 1, sizeof(char));
+	char*  buffer = (char*) calloc(n_symbols + 1, sizeof(char));
 	assert(buffer);
 
-	size_t compFread = fread(buffer, sizeof(char), simbols, in);
-	assert(compFread == simbols);
+	size_t compFread = fread(buffer, sizeof(char), n_symbols, in);
+	assert(compFread == n_symbols);
 
 	fseek(in, 0, SEEK_SET);
 
@@ -140,32 +128,32 @@ char* MakeBuffer(FILE* in, size_t simbols)
 
 size_t FillingStructs(char* buffer, line_t* arrstr)
 {
-    assert(buffer);
-    assert(arrstr);
+	if(buffer == nullptr) return MEMORY_ALLOCATION_ERROR;
+    if(arrstr == nullptr) return MEMORY_ALLOCATION_ERROR;
 
-    size_t i = 0;
+    size_t index = 0;
     size_t n_structs = 0;
 
     while(true)
     {
-        while(isspace(buffer[i])) i++;
-	    
-        arrstr[n_structs].line = &(buffer[i]);
-	    
-        while(buffer[i] != '\n')
+        while(isspace(buffer[index]))
+			index++;
+
+        arrstr[n_structs].line = &(buffer[index]);
+
+        while(buffer[index] != '\n')
         {
-		
-            if(buffer[i] == '\0')
+            if(buffer[index] == '\0')
             {
-                arrstr[n_structs].length = &(buffer[i]) - arrstr[n_structs].line;
+                arrstr[n_structs].length = &(buffer[index]) - arrstr[n_structs].line;
                 return n_structs + 1;
             }
-		
-            i++;
-		
+
+            index++;
         }
-	    
-        arrstr[n_structs].length = &(buffer[i]) - arrstr[n_structs].line;
+
+        arrstr[n_structs].length = &(buffer[index]) - arrstr[n_structs].line;
+
         n_structs++;
     }
 }
@@ -173,18 +161,21 @@ size_t FillingStructs(char* buffer, line_t* arrstr)
 //-----------------------------------------------------------------------------
 
 size_t SortFile(FILE* in, FILE* out, const char* mode)
-{//assert files
-    size_t simbols = NumberOfSimbols(in);
-    if(!simbols) return CHARACTER_COUNT_ERROR;
+{
+	if(!in ) return FILE_OPENING_ERROR;
+	if(!out) return FILE_OPENING_ERROR;
 
-	char* buffer = MakeBuffer(in, simbols);
+    size_t n_symbols = NumberOfSimbols(in);
+    if(!n_symbols) return CHARACTER_COUNT_ERROR;
+
+	char* buffer = MakeBuffer(in, n_symbols);
 	if(buffer == nullptr) return MEMORY_ALLOCATION_ERROR;
 
-    size_t strings = NumberOfStrings(simbols, buffer);
-	if(!strings) return STRINGS_COUNT_ERROR;
+    size_t n_strings = NumberOfStrings(n_symbols, buffer);
+	if(!n_strings) return STRINGS_COUNT_ERROR;
 
-    line_t* arrstr = (line_t*) calloc(sizeof(line_t), strings);
-    if(buffer == nullptr) return MEMORY_ALLOCATION_ERROR;
+    line_t* arrstr = (line_t*) calloc(sizeof(line_t), n_strings);
+    if(arrstr == nullptr) return MEMORY_ALLOCATION_ERROR;
 
     size_t n_structs = FillingStructs(buffer, arrstr);
     if(n_structs == 1) return FILLING_STRUCTURES_ERROR;
@@ -224,19 +215,18 @@ int (*choose_cmp(const char* mode))(const void*, const void*)
     size_t length1 = parg1->length;
     size_t length2 = parg2->length;
 
-    size_t min = length1 <= length2 ? length1 : length2;
-	 
-    for(size_t i = 0; i < min; i++)
+    size_t min_length = length1 <= length2 ? length1 : length2;
+
+    for(size_t index = 0; index < min_length; index++)
     {
-        if(parg1->line[i] != parg2->line[i])
-            return parg1->line[i] - parg2->line[i];
+        if(parg1->line[index] != parg2->line[index])
+            return parg1->line[index] - parg2->line[index];
     }
-	 
+
     if(length1 == length2)
         return 0;
     else
         return length1 - length2;
- //   return strcmp(parg1->line, parg2->line);
  }
 
 //-----------------------------------------------------------------------------
@@ -249,18 +239,18 @@ int (*choose_cmp(const char* mode))(const void*, const void*)
     size_t length1 = parg1->length;
     size_t length2 = parg2->length;
 
-    size_t min = length1 <= length2 ? length1 : length2;
-    for(size_t i = 0; i < min; i++)
+    size_t min_length = length1 <= length2 ? length1 : length2;
+
+    for(size_t index = 0; index < min_length; index++)
     {
-        if(parg1->line[i] != parg2->line[i])
-            return parg2->line[i] - parg1->line[i];
+        if(parg1->line[index] != parg2->line[index])
+            return parg2->line[index] - parg1->line[index];
     }
+
     if(length1 == length2)
         return 0;
     else
         return length2 - length1;
-
-//   return strcmp(parg2->line, parg1->line);
  }
 
 //-----------------------------------------------------------------------------
@@ -273,22 +263,23 @@ int rhyme_cmp(const void* arg1, const void* arg2)
 	size_t length1 = parg1->length;
 	size_t length2 = parg2->length;
 
-	size_t min = length1 <= length2 ? length1 : length2;
+	size_t min_length = length1 <= length2 ? length1 : length2;
 
-	size_t i = 0;//CharIndexFirstLine
-	size_t j = 0;
+	size_t index_first  = 0;
+	size_t index_second = 0;
 
-	while(i < min && j < min)
+	while(index_first < min_length && index_second < min_length)
 	{
-        while(isspace(parg1->line[length1 - i]))
-            i++;
-        while(isspace(parg1->line[length1 - j]))
-            j++;
+        while(isspace(parg1->line[length1 - index_first ]))
+            index_first++ ;
+        while(isspace(parg1->line[length1 - index_second]))
+            index_second++;
 
-		if(parg1->line[length1 - i] != parg2->line[length2 - i])
-                return parg1->line[length1 - i] - parg2->line[length2 - i];
-        i++;
-        j++;
+		if(parg1->line[length1 - index_first] != parg2->line[length2 - index_second])
+            return parg1->line[length1 - index_first] - parg2->line[length2 - index_second];
+
+        index_first++;
+        index_second++;
 	}
     return 0;
 }
@@ -296,10 +287,9 @@ int rhyme_cmp(const void* arg1, const void* arg2)
 //-----------------------------------------------------------------------------
 
 inline void print_structs(line_t* arrstr, size_t n_structs, FILE* out)
-{//переписать
-    for(size_t j = 0; j < n_structs; j++)//посимвольный вывод - гавно
-        for(size_t i = 0; i < arrstr[j].length; i++)
-            fprintf(out, "%c", arrstr[j].line[i]);//fprintf - гавно. Меняем на fwrite
+{
+	  for(size_t index = 0; index < n_structs; index++)
+            fwrite(arrstr[index].line, n_structs, sizeof(arrstr), out);
 }
 
 //-----------------------------------------------------------------------------
@@ -311,4 +301,6 @@ inline void free_memory(line_t* arrstr, char* buffer)
 }
 
 //-----------------------------------------------------------------------------
+
+
 
